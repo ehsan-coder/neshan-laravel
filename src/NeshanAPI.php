cfedebb2642c3be719497e4af5fe06245b65f378
Ehsan <ehsan1374000@gmail.com>
2022-09-26 02:02:43 +0330

    Added 'NeshanAPI' service

---
+++ src/NeshanAPI.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace EhsanCoder\NeshanLaravel;
+
+use Illuminate\Support\Facades\Http;
+
+class NeshanAPI
+{
+    protected $apiKey;
+    private $url;
+
+    public function __construct($apiKey,$url) {
+        if (is_null($apiKey)) {
+            die('apiKey has not been set');
+            exit;
+        }
+        $this->apiKey = $apiKey;
+        $this->url    = $url;
+    }
+
+    /**
+     * @param  string  $address
+     * @param  int  $timeout
+     *
+     * @return array|mixed
+     */
+    public function geocoding(string $address, int $timeout = 5)
+    {
+        return (Http::timeout($timeout)->withHeaders(['Api-Key' => $this->apiKey])
+            ->get($this->url.'v4/geocoding', [
+                "address" => $address,
+            ]))->json();
+    }
+
+    /**
+     * @param  string  $latitude
+     * @param  string  $longitude
+     * @param  int  $timeout
+     *
+     * @return string
+     */
+    public function reverseGeocoding(string $latitude, string $longitude, int $timeout = 5)
+    {
+        return (Http::timeout($timeout)->withHeaders(['Api-Key' => $this->apiKey])
+            ->get($this->url . 'v5/reverse', [
+            "lat" => $latitude,
+            "lng" => $longitude
+        ]))->json();
+    }
+
+    /**
+     * @param  string  $term
+     * @param  string  $latitude
+     * @param  string  $longitude
+     * @param  int  $timeout
+     *
+     * @return array|mixed
+     */
+    public function search(string $term, string $latitude, string $longitude, int $timeout = 5)
+    {
+        return (Http::timeout($timeout)->withHeaders(['Api-Key' => $this->apiKey])
+            ->get($this->url.'v1/search', [
+                "term" => $term,
+                "lat"  => $latitude,
+                "lng"  => $longitude
+            ]))->json();
+    }
+
+    public function distanceMatrix(string $origins, string $destinations, string $type = 'car', int $timeout = 5)
+    {
+        return (Http::timeout($timeout)->withHeaders(['Api-Key' => $this->apiKey])
+            ->get($this->url.'v1/distance-matrix', [
+                "type"          => $type,
+                "origins"       => $origins,
+                "destinations"  => $destinations
+            ]))->json();
+    }
+
+    /**
+     * @param  string  $path
+     * @param  int  $timeout
+     *
+     * @return array|mixed
+     */
+    public function mapMatching(string $path, int $timeout = 5)
+    {
+        return (Http::timeout($timeout)->withHeaders(['Api-Key' => $this->apiKey])
+            ->get($this->url.'v1/map-matching', [
+                "path" => $path,
+            ]))->json();
+    }
+}
